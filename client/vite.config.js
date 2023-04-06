aa92cf50a48eea8ca2a9cd6f612610e9eb3081d3
MrTofuuu <chris.stallcup@gmail.com>
2023-04-06 00:56:14 -0500

    initial commit

---
+++ client/vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy:{
+      "/graphql":{
+        target:"http://localhost:3001/",
+        changeOrigin:true,
+        secure:false,
+      }
+    }
+  },
+  // below code is if a users keeps the files as js instead of converting all to jsx
+  // esbuild: {
+  //   loader: "tsx",
+  //   include: [
+  //     // Business as usual for .jsx and .tsx files
+  //     "src/**/*.jsx",
+  //     "src/**/*.tsx",
+  //     "node_modules/**/*.jsx",
+  //     "node_modules/**/*.tsx",
+
+  //     // Add the specific files you want to allow JSX syntax in
+  //     "src/LocalJsxInJsComponent.js",
+  //     "node_modules/bad-jsx-in-js-component/js/BadJSXinJS.js",
+  //     "node_modules/bad-jsx-in-js-component/ts/BadTSXInTs.ts",
+
+  //     // --- OR ---
+
+  //     // Add these lines to allow all .js files to contain JSX
+  //     "src/**/*.js",
+  //     "node_modules/**/*.js",
+
+  //     // Add these lines to allow all .ts files to contain JSX
+  //     "src/**/*.ts",
+  //     "node_modules/**/*.ts",
+  //   ],
+  // }
+})
